adf5c86aec75545da061a922beeb70dd9232ba3b
Bozhao Liu <222460@usn.no>
2019-03-28 16:19:01 +0100

    #79 create servo class for steering and motor

--- Servo.py
+++ Servo.py
@@ -1,40 +1,84 @@
+import threading
 from time import sleep
 
 class servo:
 
-    pin = 0
-    dir = ''
-    high_time = 0.0015 # 0.0012----0.0018
-
-    def __init__(self, pin, dir = '/sys/class/gpio/'):
-        self.pin = pin
+    Pin = 0
+    Dir = ''
+    High_time = 0.0015 # 0.0012----0.0018
+    Rate = 0
+    thread = ''
+    Stoped = True
+    Test = False
+    
+    def __init__(self, pin, dir = '/sys/class/gpio/', test = False):
+        self.Pin = pin
         self.__export_pin(dir)
+        self.thread = threading.Thread(target=self.__set__pwm, args=())
+        
+        
+    def __del__(self):
+        self.Stop()
+        print("stopped")
 
     def __export_pin(self, dir):
         f = open(dir + "export", "w+")
-        f.write("%d", self.pin)
+        f.write(str(self.Pin))
         f.close()
-        self.dir = dir + "gpio" + pin + "/"
-        f = open(self.dir + "direction", "w+")
+        ## initialize GPIO
+        
+        self.Dir = dir + "gpio" + str(self.Pin) + "/"
+        f = open(self.Dir + "direction", "w+")
         f.write("out")
         f.close()
+        #Set GPIO to output mode
 
     def __set__value(self, value):
-        f = open(self.dir + "value", "w+")
+        f = open(self.Dir + "value", "w+")
         f.write(value)
         f.close()
 
-    def __set__pwm(self, ratio):
-        high_time = 0.002
-        self.__set__value(1)
-        sleep(high_time)
-        self.__set__value(0)
-        sleep(0.05-high_time)
+    def __set__pwm(self):
         
-    def Set_High_Time(self, High_time)
+        if self.Test:
+            while(1):
+                f = open(self.Dir + "value", "w+")
+                f.write(str(self.High_time + self.Rate))
+                f.close()
+                
+        else:
+            while(1):
+                self.__set__value(1)
+                sleep(self.High_time + self.Rate)
+                self.__set__value(0)
+                sleep(0.02-self.High_time - self.Rate)
+                if self.Stoped:
+                    return
+        
+        
+    def Run(self):
+        self.Stoped = False
+        self.thread.start()
+        
+    def Stop(self):
+        self.Stoped = True
+        self.thread.join()
+        
+        
+    def __Set_High_Time(self, rate):
+        if rate > 10:
+            rate = 10
+        if rate < -10:
+            rate = -10
+        self.Rate = 0.00003 * rate
+        self.thread.join()
+        
 
 
-motor = servo(388)
-while True:
-    motor.set__pwm(1)
+#motor = servo(388,"E:/")
+#motor.Run()
+#motor.Stop()
+#sleep(3)
+#del motor
+
 
